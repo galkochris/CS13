113bc8162a7bf8efed36dfc64710751b913445d6
galkochris <galkochris@gmail.com>
2020-03-04 13:17:16 -0800

    updates to arraystack

--- Code/stack.py
+++ Code/stack.py
@@ -74,10 +74,10 @@ class ArrayStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
-        if self.length == 0:
+        if self.length() == 0:
             return True
         else:
-            False
+            return False
 
     def length(self):
         """Return the number of items in this stack."""
@@ -94,14 +94,20 @@ class ArrayStack(object):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
         # TODO: Return top item, if any
-        return self.list[self.length - 1]
+        if self.is_empty():
+            return None
+        else:
+            return self.list[-1]
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return top item, if any
-        return self.peek & self.list.pop(self.length - 1)
+        if self.is_empty():
+            raise ValueError('there is nothing here')
+        else:
+            return self.peek() and self.list.pop(- 1)
 
 
 # Implement LinkedStack and ArrayStack above, then change the assignment below
